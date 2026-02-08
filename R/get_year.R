733837d3e28f4f721e245da0c47c5e69fe8efa87
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-02-07 20:55:50 -0500

    url argument

--- R/get_year.R
+++ R/get_year.R
@@ -1,18 +1,19 @@
-datasets_url_prefix <-
-  "http://www2.census.gov/programs-surveys/nsch/datasets"
-get_year <- function(year, data.dir=tempdir()){
-  if(!(
-    is.integer(year) && length(year)==1 && is.finite(year)
-  ))stop("year must be a single finite integer")
-  year.zip <- sprintf("nsch_%d_topical_Stata.zip", year)
+get_year <- function(year_url, data.dir=tempdir()){
+  year.html <- basename(year_url)
+  data.dir.year.html <- file.path(data.dir, year.html)
+  if(!file.exists(data.dir.year.html)){
+    download.file(year_url, data.dir.year.html)
+  }
+  url.dt <- nc::capture_all_str(
+    data.dir.year.html,
+    url="//.*?topical_Stata[.]zip")
+  if(nrow(url.dt)==0)
+    stop("no topical_Stata.zip urls on ", year_url)
+  http_url <- paste0("http:", url.dt$url)
+  year.zip <- basename(http_url)
   data.dir.year.zip <- file.path(data.dir, year.zip)
   if(!file.exists(data.dir.year.zip)){
-    u <- sprintf(
-      "%s/%d/%s",
-      datasets_url_prefix,
-      year,
-      year.zip)
-    download.file(u, data.dir.year.zip)
+    download.file(http_url, data.dir.year.zip)
   }
-  unzip(year.zip, exdir=data.dir)
+  unzip(data.dir.year.zip, exdir=data.dir)
 }
