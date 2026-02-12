07e49ff4fde35dc3d25857f70c5174a6090687c4
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-02-12 16:17:29 -0500

    data-2024

--- R/get_year.R
+++ R/get_year.R
@@ -1,19 +1,19 @@
-get_year <- function(year_url, data.dir=tempdir()){
+get_year <- function(year_url, data.path=tempdir()){
   year.html <- basename(year_url)
-  data.dir.year.html <- file.path(data.dir, year.html)
-  if(!file.exists(data.dir.year.html)){
-    download.file(year_url, data.dir.year.html)
+  data.path.year.html <- file.path(data.path, year.html)
+  if(!file.exists(data.path.year.html)){
+    download.file(year_url, data.path.year.html)
   }
   url.dt <- nc::capture_all_str(
-    data.dir.year.html,
+    data.path.year.html,
     url="//.*?topical_Stata[.]zip")
   if(nrow(url.dt)==0)
     stop("no topical_Stata.zip urls on ", year_url)
   http_url <- paste0("http:", url.dt$url)
   year.zip <- basename(http_url)
-  data.dir.year.zip <- file.path(data.dir, year.zip)
-  if(!file.exists(data.dir.year.zip)){
-    download.file(http_url, data.dir.year.zip)
+  data.path.year.zip <- file.path(data.path, year.zip)
+  if(!file.exists(data.path.year.zip)){
+    download.file(http_url, data.path.year.zip)
   }
-  unzip(data.dir.year.zip, exdir=data.dir)
+  unzip(data.path.year.zip, exdir=data.path)
 }
