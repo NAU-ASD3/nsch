6d58067f4fdc92fbc1cbc15752b359f8f48381dd
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-02-12 14:44:07 -0500

    expect error

--- tests/testthat/test-parse_do.R
+++ tests/testthat/test-parse_do.R
@@ -13,3 +13,10 @@ test_that("parse_do #rows consistent with grep #lines", {
   }, by=data_type]
   rows_dt[, expect_identical(computed, expected)]
 })
+
+test_that("informative error for missing do file", {
+  does.not.exist <- tempfile()
+  expect_error({
+    nsch::parse_do(does.not.exist)
+  }, paste("year.do.path should be the path to a Stata do file, but this file does not exist:", does.not.exist)
+})
