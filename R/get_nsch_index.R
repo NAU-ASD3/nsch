5ef28ffe733982f3d1b9958a77b500001303774d
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-02-12 14:56:57 -0500

    .path

--- R/get_nsch_index.R
+++ R/get_nsch_index.R
@@ -1,7 +1,7 @@
 nsch_url_prefix <- "https://www.census.gov/programs-surveys/nsch/data/datasets."
 nsch_data_url <- paste0(nsch_url_prefix, "html")
 get_nsch_index <- function(local_html.path = tempfile()){
-  if(!file.exists(local_html)){
+  if(!file.exists(local_html.path)){
     download.file(nsch_data_url, local_html.path, quiet = TRUE)
   }
   ## capture one or more digits, convert to integer.
