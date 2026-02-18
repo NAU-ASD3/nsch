f6943f10b52d7e979d0bd867bca1f6481115d941
Chris Reger <kcr28@nau.edu>
2026-02-18 11:33:56 -0700

    transform_values remaps numeric values; rename_vars renames columns; merge_vars coalesces paired columns; subset_vars selects desired columns

--- R/merge_vars.R
+++ R/merge_vars.R
@@ -1,3 +1,34 @@
 merge_vars <- function(dt, merges, year){
-  stop("not yet implemented")
+  for(variable.name in names(merges)){
+    details <- merges[[variable.name]]
+    col1 <- details$column_1
+    col2 <- details$column_2
+    if(as.character(year) %in% details$years &&
+       col1 %in% names(dt) &&
+       col2 %in% names(dt)){
+      ## Coalesce data columns: prefer column_1 where non-NA.
+      vec1 <- dt[[col1]]
+      vec2 <- dt[[col2]]
+      merged.vals <- ifelse(is.na(vec1), vec2, vec1)
+      data.table::set(dt, j = variable.name, value = as.numeric(merged.vals))
+      ## Coalesce _label companion columns if they exist.
+      label1 <- paste0(col1, "_label")
+      label2 <- paste0(col2, "_label")
+      label.col <- paste0(variable.name, "_label")
+      if(label1 %in% names(dt) && label2 %in% names(dt)){
+        lab1 <- dt[[label1]]
+        lab2 <- dt[[label2]]
+        data.table::set(
+          dt, j = label.col,
+          value = ifelse(is.na(vec1), lab2, lab1))
+      }
+      ## Remove original source columns and their _label companions.
+      for(col in c(col1, col2, label1, label2)){
+        if(col %in% names(dt)){
+          data.table::set(dt, j = col, value = NULL)
+        }
+      }
+    }
+  }
+  invisible(dt)
 }
--- R/transform_values.R
+++ R/transform_values.R
@@ -1,3 +1,24 @@
 transform_values <- function(dt, transforms, year){
-  stop("not yet implemented")
+  for(variable.name in names(transforms)){
+    details <- transforms[[variable.name]]
+    if(as.character(year) %in% details$years &&
+       variable.name %in% names(dt)){
+      label.col <- paste0(variable.name, "_label")
+      ## Initialize _label column if it does not yet exist.
+      if(!(label.col %in% names(dt))){
+        data.table::set(dt, j = label.col, value = NA_character_)
+      }
+      for(i in seq_along(details$value)){
+        old.val <- as.numeric(details$value[i])
+        new.val <- as.numeric(details$new_value[i])
+        new.label <- details$new_label[i]
+        rows <- which(dt[[variable.name]] == old.val)
+        if(length(rows) > 0){
+          data.table::set(dt, i = rows, j = variable.name, value = new.val)
+          data.table::set(dt, i = rows, j = label.col, value = new.label)
+        }
+      }
+    }
+  }
+  invisible(dt)
 }
