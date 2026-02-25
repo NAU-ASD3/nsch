1b1f46ef1c9d64d7cd592a310928a89837ca53ac
Sai charan <sk3949@nau.edu>
2026-02-25 13:42:42 -0700

    Apply suggestion from @tdhock
    Co-authored-by: Toby Dylan Hocking <toby.hocking@r-project.org>

--- R/get_year.R
+++ R/get_year.R
@@ -1,13 +1,11 @@
  get_year <- function(year_url, data.path=file.path("NSCH_data", "00_original_Stata")){
   ##Input validation
   if(!is.character(year_url) || length(year_url) != 1){
-    stop("`year_url` must be a single character string.")
+    stop("year_url must be a single character string")
   }
-
   if(!is.character(data.path) || length(data.path) != 1){
-    stop("`data.path` must be a single character string.")
+    stop("data.path must be a single character string")
   }
-
   dir.create(data.path, recursive = TRUE, showWarnings = FALSE)
   year.html <- basename(year_url)
   data.path.year.html <- file.path(data.path, year.html)
