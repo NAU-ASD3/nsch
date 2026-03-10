a786fad6549e034637719d45f5728216496f6f62
Chris Reger <kcr28@nau.edu>
2026-03-10 16:43:38 -0700

    transform_values: vectorize inner loop with match()

--- R/transform_values.R
+++ R/transform_values.R
@@ -1,22 +1,26 @@
-transform_values <- function(dt, transforms, year){
-  for(variable.name in names(transforms)){
+transform_values <- function(dt, transforms, year) {
+  for (variable.name in names(transforms)) {
     details <- transforms[[variable.name]]
-    if(as.character(year) %in% details$years &&
-       variable.name %in% names(dt)){
+    if (as.character(year) %in% details$years &&
+        variable.name %in% names(dt)) {
       label.col <- paste0(variable.name, "_label")
       ## Initialize _label column if it does not yet exist.
-      if(!(label.col %in% names(dt))){
+      if (!(label.col %in% names(dt))) {
         data.table::set(dt, j = label.col, value = NA_character_)
       }
-      for(i in seq_along(details$value)){
-        old.val <- as.numeric(details$value[i])
-        new.val <- as.numeric(details$new_value[i])
-        new.label <- details$new_label[i]
-        rows <- which(dt[[variable.name]] == old.val)
-        if(length(rows) > 0){
-          data.table::set(dt, i = rows, j = variable.name, value = new.val)
-          data.table::set(dt, i = rows, j = label.col, value = new.label)
-        }
+      old.vals <- as.numeric(details$value)
+      new.vals <- as.numeric(details$new_value)
+      idx <- match(dt[[variable.name]], old.vals)
+      matched <- which(!is.na(idx))
+      if (length(matched) > 0) {
+        data.table::set(dt,
+                        i = matched,
+                        j = variable.name,
+                        value = new.vals[idx[matched]])
+        data.table::set(dt,
+                        i = matched,
+                        j = label.col,
+                        value = details$new_label[idx[matched]])
       }
     }
   }
