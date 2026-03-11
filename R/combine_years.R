2f29503a0d474e6ea51214dd99e87445c9b9bada
Chris Reger <kcr28@nau.edu>
2026-03-10 17:33:51 -0700

    combine_years row-binds yearly data.tables with validation

--- R/combine_years.R
+++ R/combine_years.R
@@ -1,3 +1,17 @@
-combine_years <- function(year.list){
-  stop("not yet implemented")
+combine_years <- function(year.list) {
+  if (!is.list(year.list) || length(year.list) == 0) {
+    stop("year.list must be a non-empty list of data.tables")
+  }
+  for (i in seq_along(year.list)) {
+    if (!("year" %in% names(year.list[[i]]))) {
+      stop("element ", i, " of year.list does not contain a 'year' column")
+    }
+  }
+  all.years <- unlist(lapply(year.list, function(dt)
+    unique(dt[["year"]])))
+  if (anyDuplicated(all.years)) {
+    stop("duplicate year values across tables: ",
+         paste(all.years[duplicated(all.years)], collapse = ", "))
+  }
+  data.table::rbindlist(year.list, use.names = TRUE, fill = TRUE)
 }
