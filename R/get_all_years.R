6b1780fa17769873ea1d333fdbd1e15cf92c5763
Chris Reger <kcr28@nau.edu>
2026-03-30 11:05:55 -0700

    add get_all_years and get_clean_data stubs and export

---
+++ R/get_all_years.R
@@ -0,0 +1,8 @@
+year <- NULL
+get_all_years <- function(
+    data.path = file.path("NSCH_data", "00_original_Stata"),
+    years = NULL,
+    download = FALSE
+) {
+  stop("not yet implemented")
+}
---
+++ R/get_clean_data.R
@@ -0,0 +1,11 @@
+get_clean_data <- function(
+    config.path = system.file("extdata", "variable-config.json", package = "nsch"),
+    data.path = file.path("NSCH_data", "00_original_Stata"),
+    years = NULL,
+    download = FALSE,
+    impute.2016 = TRUE,
+    seed = 1L
+) {
+  stop("not yet implemented")
+}
+```
